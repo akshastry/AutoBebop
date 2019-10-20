02831434b3088c761ba90ff7488c4bce5a9fe182
akshastry <shastry@umd.edu>
2019-10-19 21:50:27 -0400

    EKF_bug_corrected(tab_was_space)

--- src/Kalman_Filter/EKF.py
+++ src/Kalman_Filter/EKF.py
@@ -1,8 +1,67 @@
 #!/usr/bin/env python
 
 import rospy
+from math import atan2, sin, cos, asin, acos
 import numpy as np
 from nav_msgs.msg import Odometry
+
+
+def R_QI(psi):
+	mat = np.zeros((3,3))
+	mat[2,2] = 1
+	mat[0,0] = cos(psi)
+	mat[0,1] = sin(psi)
+	mat[1,0] = -sin(psi)
+	mat[1,1] = cos(psi)
+
+	return mat
+
+def R_WC(psi):
+	mat = np.zeros((3,3))
+	mat[2,2] = 1
+	mat[0,0] = cos(psi)
+	mat[0,1] = -sin(psi)
+	mat[1,0] = sin(psi)
+	mat[1,1] = cos(psi)
+
+	return mat
+
+def delR_WC(psi):
+	mat = np.zeros((3,3))
+	mat[0,0] = -sin(psi)
+	mat[0,1] = -cos(psi)
+	mat[1,0] = cos(psi)
+	mat[1,1] = -sin(psi)
+
+	return mat
+
+
+def euler_to_quaternion(roll, pitch, yaw):
+
+		qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
+		qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
+		qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
+		qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
+
+		return [qw, qx, qy, qz]
+
+
+def quaternion_to_euler(w, x, y, z):
+
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll = atan2(t0, t1)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch = asin(t2)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = atan2(t3, t4)
+    # return [yaw, pitch, roll]
+    return [roll, pitch, yaw]
+
+
 
 
 x_prev = 0.0
@@ -44,38 +103,37 @@ def EKF_predict(delX, psi):
 	global X_k, P, Q
 
 	X_k1 = np.zeros(4)
+	R_k = np.matmul(np.matmul(R_WC(X_k[3]), R_CQ), R_QI(psi))
 
-    R_k = np.matmul(np.matmul(R_WC(X_k[3]), R_CQ), R_QI(psi))
-    # print(np.transpose(np.matmul(R_k, np.reshape(V_k, (-1, 1))) * dt))
+	X_k1[0:3] = X_k[0:3] + np.transpose(np.matmul(R_k, np.reshape(delX, (-1, 1))))[0]
+	X_k1[3] = X_k[3]
 
-    X_k1[0:3] = X_k[0:3] + np.transpose(np.matmul(R_k, np.reshape(delX, (-1, 1))))[0]
-    X_k1[3] = X_k[3]
-
-    #calculate F = df/dx
-    F = np.zeros((4,4))
-    F[0:3,0:3] = np.eye(3)
-    R_kF = np.matmul( np.matmul( delR_WC(X_k[3]), R_CQ), R_QI(psi))
-    F[0:3,3] = np.transpose(np.matmul(R_kF, np.reshape(delX, (-1, 1)) ))[0]
-    F[3,3] = 1
-
-    P = np.matmul(F, np.matmul(P, np.transpose(F))) + Q
-
-    X_k = X_k1
+	#calculate F = df/dx
+	F = np.zeros((4,4))
+	F[0:3,0:3] = np.eye(3)
+	R_kF = np.matmul( np.matmul( delR_WC(X_k[3]), R_CQ), R_QI(psi))
+	F[0:3,3] = np.transpose(np.matmul(R_kF, np.reshape(delX, (-1, 1)) ))[0]
+	F[3,3] = 1
+	
+	P = np.matmul(F, np.matmul(P, np.transpose(F))) + Q
+	
+	X_k = X_k1
 
 def EKF_update(Z_k):
 	global X_k, P, H, R
 
-    y_tilda = Z_k - np.transpose(np.matmul(H, np.reshape(X_k, (-1, 1))))[0]
+	y_tilda = Z_k - np.transpose(np.matmul(H, np.reshape(X_k, (-1, 1))))[0]
 
-    S = np.matmul(H, np.matmul(P, np.transpose(H))) + R
-    K = np.matmul(P, np.matmul(np.transpose(H), np.linalg.inv(S)))
+	S = np.matmul(H, np.matmul(P, np.transpose(H))) + R
+	K = np.matmul(P, np.matmul(np.transpose(H), np.linalg.inv(S)))
 
-    X_k1 = X_k1 + np.transpose(np.matmul(K, np.reshape(y_tilda, (-1, 1))))[0]
-    P = np.matmul(np.eye(4) - np.matmul(K,H), P)
+	X_k1 = X_k1 + np.transpose(np.matmul(K, np.reshape(y_tilda, (-1, 1))))[0]
+	P = np.matmul(np.eye(4) - np.matmul(K,H), P)
 
-    X_k = X_k1
+	X_k = X_k1
 
 def odom_callback(data):
+	global x_prev, y_prev, z_prev
 	x = data.pose.pose.position.x
 	y = data.pose.pose.position.y
 	z = data.pose.pose.position.z
@@ -109,14 +167,7 @@ def win_callback(data):
 
 	Z_k = np.array([x, y, z, roll])
 
-	KF_update(Z_k)
-
-
-if __name__ == '__main__':
-    try:
-        main()
-    except rospy.ROSInterruptException:
-        pass
+	EKF_update(Z_k)
 
 
 def main():
@@ -147,7 +198,7 @@ def main():
 		q0, q1, q2, q3 = euler_to_quaternion(0, 0, X_k[3])
 
 		pose_EKF = Odometry()
-		pose_EKF.header.frame_id = "pose_rel_win_filtered"
+		pose_EKF.header.frame_id = "odom"
 		pose_EKF.child_frame_id = "base_link"
 		pose_EKF.header.stamp = rospy.get_rostime()
 		pose_EKF.pose.pose.position.x = X_k[0]
@@ -172,58 +223,15 @@ def main():
 		
 	# 	rate.sleep()
 
-def euler_to_quaternion(roll, pitch, yaw):
-
-		qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
-		qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
-		qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
-		qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
-
-		return [qw, qx, qy, qz]
-
-
-def quaternion_to_euler(w, x, y, z):
-
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + y * y)
-    roll = atan2(t0, t1)
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    pitch = asin(t2)
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (y * y + z * z)
-    yaw = atan2(t3, t4)
-    # return [yaw, pitch, roll]
-    return [roll, pitch, yaw]
 
 
 
-def R_QI(psi):
-	mat = np.zeros((3,3))
-	mat[2,2] = 1
-	mat[0,0] = cos(psi)
-	mat[0,1] = sin(psi)
-	mat[1,0] = -sin(psi)
-	mat[1,1] = cos(psi)
 
-	return mat
 
-def R_WC(psi):
-	mat = np.zeros((3,3))
-	mat[2,2] = 1
-	mat[0,0] = cos(psi)
-	mat[0,1] = -sin(psi)
-	mat[1,0] = sin(psi)
-	mat[1,1] = cos(psi)
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
 
-	return mat
 
-def delR_WC(psi):
-	mat = np.zeros((3,3))
-	mat[0,0] = -sin(psi)
-	mat[0,1] = -cos(psi)
-	mat[1,0] = cos(psi)
-	mat[1,1] = -sin(psi)
-
-	return mat
