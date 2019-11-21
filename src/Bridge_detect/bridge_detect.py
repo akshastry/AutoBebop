b8f29697ca43d4cf364a79946649474637ff714e
nickrehm <nrehm@umd.edu>
2019-11-21 00:33:59 -0500

    tuned bridge detection in bridge_detect.py

--- src/Bridge_detect/bridge_detect.py
+++ src/Bridge_detect/bridge_detect.py
@@ -26,7 +26,7 @@ obs_offset_y = 0.0
 obs_offset_z = 0.0
 
 #bridge coordinates in body frame
-cX_bridge_prev = cY_bridge_prev = xb_past_prev = yb_past_prev = xb_b4_prev = yb_b4_prev = 0.0
+cX_bridge_prev = cY_bridge_prev = xb_past_prev = yb_past_prev = xb_b4_prev = yb_b4_prev = theta_prev = 0.0
 
 # current state of quad
 x = y = z = vx = vy = vz = roll = pitch = yaw = 0.0
@@ -139,9 +139,7 @@ def quad_pose(data):
 	q3 = data.pose.pose.orientation.z
 	roll, pitch, yaw = quaternion_to_euler(q0, q1, q2, q3)
 
-
 def quaternion_to_euler(w, x, y, z):
-
 	t0 = +2.0 * (w * x + y * z)
 	t1 = +1.0 - 2.0 * (x * x + y * y)
 	roll = atan2(t0, t1)
@@ -155,18 +153,17 @@ def quaternion_to_euler(w, x, y, z):
 	# return [yaw, pitch, roll]
 	return [roll, pitch, yaw]
 
-
 def euler_to_quaternion(roll, pitch, yaw):
 
     qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
     qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
     qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2)
     qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2)
-
     return [qw, qx, qy, qz]
 
 ###################################################
 #NEW FUNCTIONS UNIQUE TO BRIDGE MISSION
+
 def scaleImage(frame):
     global width, height
     #Resize image
@@ -179,12 +176,12 @@ def scaleImage(frame):
 
 def getEdges(frame):
     #find edges in image
-    frame = cv2.Canny(frame,0,200) #tune second parameter for more/less agressive detection
+    frame = cv2.Canny(frame,0,150) #tune second parameter for more/less agressive detection
     return frame
 
 def firstDilate(frame):
     #dilate/erode edges
-    kernel = np.ones((8,8), np.uint8)
+    kernel = np.ones((5,5), np.uint8)
     frame = cv2.dilate(frame, kernel, iterations=1)
     frame = cv2.bitwise_not(frame)
     # kernel = np.ones((12,12), np.uint8)
@@ -197,14 +194,14 @@ def featurelessThresh(frame):
     #simple threshhold on featureless areas; if featureless area is too dark, likely not bridge
 	frame[np.where(frame == 255)] = orig_frame[np.where(frame == 255)]
     #threshhold for high brightness featurelss areas
-	lower = 100 #lower threshhold value
+	lower = 80 #lower threshhold value
 	upper = 255 #upper threshhold value
 	frame = cv2.inRange(frame, lower, upper)
    	#dilate/erode edges
-	# kernel = np.ones((5,5), np.uint8)
-	# frame = cv2.dilate(frame, kernel, iterations=1) 
-	# kernel = np.ones((5,5), np.uint8)
-	# frame = cv2.erode(frame, kernel, iterations=1)
+	kernel = np.ones((2,2), np.uint8)
+	frame = cv2.dilate(frame, kernel, iterations=1) 
+	kernel = np.ones((3,3), np.uint8)
+	frame = cv2.erode(frame, kernel, iterations=1)
 	return frame
 
 def fillBridge(frame,areas,idxs,cntsSorted):
@@ -232,7 +229,7 @@ def fitPoly(frame,hull):
 
 def getBridgeCenter(frame):
     #find 4 corners of bridge and sort them clockwise from top left corner
-    corners = cv2.goodFeaturesToTrack(frame,4,0.01,10) #pick out 4 most prominant corners
+    corners = cv2.goodFeaturesToTrack(frame,4,0.01,80) #pick out 4 most prominant corners
     #draw circles
     for corner in corners:
         x,y = corner.ravel()
@@ -267,14 +264,21 @@ def getBridgeCenter(frame):
     d2 = math.sqrt((corner_sort_x[3] - corner_sort_x[0])**2 + (corner_sort_y[3] - corner_sort_y[0])**2)
     d3 = math.sqrt((corner_sort_x[3] - corner_sort_x[2])**2 + (corner_sort_y[3] - corner_sort_y[2])**2)
     d4 = math.sqrt((corner_sort_x[2] - corner_sort_x[1])**2 + (corner_sort_y[2] - corner_sort_y[1])**2)
-    if d1 > d2: #horizontal
-        slope1 = (corner_sort_y[1] - corner_sort_y[0])/(corner_sort_x[1] - corner_sort_x[0])
-        slope2 = (corner_sort_y[3] - corner_sort_y[2])/(corner_sort_x[3] - corner_sort_x[2])
-        slope  = (slope1 + slope2)/2
-    if d1 < d2: #vertical
-        slope1 = (corner_sort_y[3] - corner_sort_y[0])/(corner_sort_x[3] - corner_sort_x[0])
-        slope2 = (corner_sort_y[2] - corner_sort_y[1])/(corner_sort_x[2] - corner_sort_x[1])
-        slope  = (slope1 + slope2)/2  
+    #check if bridge is at top or bottom of frame
+    if np.sum(frame[0,:])>0:
+    	slope = 100000
+    elif np.sum(frame[height-1,:])>0:
+    	slope = -100000
+    #get slope normally if bridge is in frame normally
+    else:
+	    if d1 > d2: #horizontal
+	        slope1 = (corner_sort_y[1] - corner_sort_y[0])/(corner_sort_x[1] - corner_sort_x[0])
+	        slope2 = (corner_sort_y[3] - corner_sort_y[2])/(corner_sort_x[3] - corner_sort_x[2])
+	        slope  = (slope1 + slope2)/2
+	    if d1 < d2: #vertical
+	        slope1 = (corner_sort_y[3] - corner_sort_y[0])/(corner_sort_x[3] - corner_sort_x[0])
+	        slope2 = (corner_sort_y[2] - corner_sort_y[1])/(corner_sort_x[2] - corner_sort_x[1])
+	        slope  = (slope1 + slope2)/2  
     return frame,cX_bridge,cY_bridge,slope
 
 def getBridgeWaypoints(orig_frame,slope,cX_bridge,cY_bridge):
@@ -288,27 +292,27 @@ def getBridgeWaypoints(orig_frame,slope,cX_bridge,cY_bridge):
     #quadrant 1
     if cY_bridge < height/2 and cX_bridge < width/2:
         if slope > 0:
-            signX = 1.0
+            signX = -1.0
             signY = -1.0
         if slope < 0:
-            signX = -1.0
+            signX = 1.0
             signY = -1.0
     #quadrant 2
     if cY_bridge < height/2 and cX_bridge > width/2:
         if slope > 0:
-            signX = 1.0
-            signY = -1.0
-        if slope < 0:
-            signX = 1.0
+            signX = -1.0
             signY = 1.0
+        if slope < 0:
+            signX = -1.0
+            signY = -1.0
     #quadrant 3
     if cY_bridge > height/2 and cX_bridge < width/2:
         if slope > 0:
             signX = -1.0
             signY = 1.0
         if slope < 0:
-            signX = -1.0
-            signY = -1.0  
+            signX = 1.0
+            signY = 1.0  
     #quadrant 4
     if cY_bridge > height/2 and cX_bridge > width/2:
         if slope > 0:
@@ -342,15 +346,19 @@ def pixel2meters(cX_bridge,cY_bridge,x_past,y_past,x_b4,y_b4):
     return cX_bridge,cY_bridge,xb_past,yb_past,xb_b4,yb_b4 
 
 def lowPassWaypoints(cX_bridge,cY_bridge,xb_past,yb_past,xb_b4,yb_b4,cX_bridge_prev,cY_bridge_prev,xb_past_prev,yb_past_prev,xb_b4_prev,yb_b4_prev):
-    B = 0.5 #lp filter parameter
-    cX_bridge = B*cX_bridge + (1-B)*cX_bridge_prev
-    cY_bridge = B*cY_bridge + (1-B)*cY_bridge_prev
-    xb_past = B*xb_past + (1-B)*xb_past_prev
-    yb_past = B*yb_past + (1-B)*yb_past_prev
-    xb_b4 = B*xb_b4 + (1-B)*xb_b4_prev
-    yb_b4 = B*yb_b4 + (1-B)*yb_b4_prev
+    B = 0.1 #lp filter parameter
+    cX_bridge = B*cX_bridge + (1.0-B)*cX_bridge_prev
+    cY_bridge = B*cY_bridge + (1.0-B)*cY_bridge_prev
+    xb_past = B*xb_past + (1.0-B)*xb_past_prev
+    yb_past = B*yb_past + (1.0-B)*yb_past_prev
+    xb_b4 = B*xb_b4 + (1.0-B)*xb_b4_prev
+    yb_b4 = B*yb_b4 + (1.0-B)*yb_b4_prev
     return cX_bridge,cY_bridge,xb_past,yb_past,xb_b4,yb_b4
 
+def lowPassTheta(theta,theta_prev):
+	B = 0.1
+	theta = B*theta + (1.0 - B)*theta_prev
+	return theta
 
 
 #############
@@ -373,7 +381,7 @@ def main():
 	rospy.Subscriber('/duo3d/left/image_rect', Image, callback) #should be faster than 20Hz or the rate of publishing below, else EKF might get fucked up
 	rospy.Subscriber('/pose_in', Odometry, quad_pose)
 
-	cX_bridge_prev = cY_bridge_prev = xb_past_prev = yb_past_prev = xb_b4_prev = yb_b4_prev = 0.0
+	cX_bridge_prev = cY_bridge_prev = xb_past_prev = yb_past_prev = xb_b4_prev = yb_b4_prev = theta_prev = 0.0
 
 	rate = rospy.Rate(20)
 	while not rospy.is_shutdown():
@@ -389,6 +397,8 @@ def main():
 
 			#scale image if needed
 			frame = scaleImage(frame)
+			#frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+			#frame = cv2.flip(frame,0)
 			orig_frame = np.copy(frame)
 
 			#get edges of image
@@ -413,7 +423,7 @@ def main():
 			#find all contours of mask
 			_,contours,_ = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 			frame = np.zeros(shape=[height,width, 1], dtype=np.uint8)
-			max_contour_ratio = .01  #change based on relative size of bridge in frame 
+			max_contour_ratio = .02  #change based on relative size of bridge in frame 
 
 			if len(contours) > 0:
 			    #sort the contours by area
@@ -444,7 +454,7 @@ def main():
 			        #convert pixel coordinates of two waypoints to displacements w.r.t. body frame
 			        cX_bridge,cY_bridge,xb_past,yb_past,xb_b4,yb_b4 = pixel2meters(cX_bridge,cY_bridge,x_past,y_past,x_b4,y_b4 )
 			        
-			        #lowpass filter waypoint coordinates 
+			        #lowpass filter waypoint coordinates
 			        cX_bridge,cY_bridge,xb_past,yb_past,xb_b4,yb_b4 = lowPassWaypoints(cX_bridge,cY_bridge,xb_past,yb_past,xb_b4,yb_b4,cX_bridge_prev,cY_bridge_prev,xb_past_prev,yb_past_prev,xb_b4_prev,yb_b4_prev)
 			        #store previous values 
 			        cX_bridge_prev = cX_bridge
@@ -456,45 +466,22 @@ def main():
 
 			        #calculate angle
 			        theta = math.atan2((xb_past - xb_b4),(yb_past-yb_b4)) #radians
+			        theta = lowPassTheta(theta,theta_prev)
+			        theta_prev = theta
 
 			        #pose
 			        get_pose(cX_bridge,cY_bridge,theta)
-			        pose_cam2in(theta)
-			        
-			        
-			        # print('')
-			        # print('Distance to first waypoint in body frame:')
-			        # print(round(xb_b4,3),round(yb_b4,3))
-			        # print('Distance to second waypoint in body frame:')
-			        # print(round(xb_past,3),round(yb_past,3))
-			        # print('Distance to center in body frame:')
-			        # print(round(cX_bridge,3),round(cY_bridge,3))
-			        # print('theta:')
-			        # print(180.0/3.14*theta,' degrees')
-
+			        pose_cam2in(theta)     
 
 			orig_frame2 = bridge.cv2_to_imgmsg(orig_frame, "8UC1")
 			pub_orig_frame_image.publish(orig_frame2)
 
-			#convert images
-			# edges = bridge.cv2_to_imgmsg(edges, "8UC1")
-			# dilatedEdges = bridge.cv2_to_imgmsg(dilatedEdges, "8UC1")
-			# bridgemask = bridge.cv2_to_imgmsg(bridgemask, "8UC1")
-			# filledBridge = bridge.cv2_to_imgmsg(filledBridge, "8UC1")
-			# corners = bridge.cv2_to_imgmsg(np.asarray(corners), "8UC1")
-			# orig_frame = bridge.cv2_to_imgmsg(orig_frame, "8UC1")
 
 		except Exception:
 			traceback.print_exc()
 			rospy.loginfo('Some error ocurred... in target_detect.py')
 
 
-		# pub_edges_image.publish(edges)
-		# pub_dilatedEdges_image.publish(dilatedEdges)
-		# pub_bridgemask_image.publish(bridgemask)
-		# pub_filledBridge_image.publish(filledBridge)
-		# pub_corners_image.publish(corners)
-		# pub_orig_frame_image.publish(orig_frame)
 		rate.sleep()
 
 if __name__ == '__main__':
